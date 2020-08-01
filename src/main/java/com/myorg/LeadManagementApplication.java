9c8118fa5d049239cf111167055382add45b7068
BharaniMurali <mbharanimca@gmail.com>
2020-08-01 16:47:26 +0530

    SOP added
    ok

--- src/main/java/com/myorg/LeadManagementApplication.java
+++ src/main/java/com/myorg/LeadManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeadManagementApplication {
 
 	public static void main(String[] args) {
+		System.out.println("In Lead Management Main Class");
 		SpringApplication.run(LeadManagementApplication.class, args);
 	}
 
