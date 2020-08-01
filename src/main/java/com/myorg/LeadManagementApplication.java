7bdd059af07c63cc072c2744a3d09938fe25400f
naveenkumar-md <68432959+naveenkumar-md@users.noreply.github.com>
2020-08-01 17:58:33 +0530

    Update LeadManagementApplication.java

--- src/main/java/com/myorg/LeadManagementApplication.java
+++ src/main/java/com/myorg/LeadManagementApplication.java
@@ -8,6 +8,7 @@ public class LeadManagementApplication {
 
 	public static void main(String[] args) {
 		System.out.println("In Lead Management Main Class change...");
+		System.out.println("Branch change...");
 		SpringApplication.run(LeadManagementApplication.class, args);
 		System.out.println("In Lead Management Main Class End");
 		System.out.println("In Lead Management Main Class End2");
